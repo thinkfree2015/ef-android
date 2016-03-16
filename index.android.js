0dd8b3102a93b85a6e76588ba55d9030a10bf9a6
iamdurui <durui911225_>
2016-03-16 10:45:29 +0800

    4444444444

--- index.android.js
+++ index.android.js
@@ -4,11 +4,33 @@
  */
 'use strict';
 import React, {
-  AppRegistry,
-  Component,
-  Text,
-  View
+    AppRegistry,
+    Component,
+    Text,
+    View,
+    Navigator
 } from 'react-native';
-import HomeView from './appView/home/home';
+import HomeView from './appView/projects/project';
+import { Provider } from 'react-redux';
+import store from './appView/redux/store';
+export default class Start extends Component {
+    render() {
+        var defaultName = 'HomeView';
+        var defaultComponent = HomeView;
+        return (
+            <Provider store={store} key="provider">
+                <Navigator
+                    initialRoute={{ name: defaultName, component: defaultComponent }}
+                    configureScene={(route) => {
+                    return Navigator.SceneConfigs.VerticalDownSwipeJump;
+                }}
+                    renderScene={(route, navigator) => {
+                    let Component = route.component;
+                    return <Component {...route.params} navigator={navigator} />
+                }}/>
+            </Provider>
+        );
 
-AppRegistry.registerComponent('RongYiCast', () => HomeView);
+    }
+}
+AppRegistry.registerComponent('RongYiCast', () => Start);
